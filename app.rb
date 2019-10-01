e3def2f4e280aad7fea49c25fb3cb725d4c52743
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-10-01 16:56:26 +0100

    added names page and test

--- app.rb
+++ app.rb
@@ -3,9 +3,15 @@ require 'sinatra/base'
 require 'shotgun'
 
 class Battle < Sinatra::Base
-  
+
   get '/' do
-    "Hello Battle!!"
+    erb :index
+  end
+
+  post '/names' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/testing_infastructure_spec.rb
+++ spec/features/testing_infastructure_spec.rb
@@ -1,8 +1,8 @@
 # require "./app.rb"
-
-feature 'Testing infastructure' do
-  scenario 'can check page contents' do
-    visit('/')
-    expect(page).to have_content 'Battle'
-  end
-end
+# 
+# feature 'Testing infastructure' do
+#   scenario 'can check page contents' do
+#     visit('/')
+#     expect(page).to have_content 'Battle'
+#   end
+# end
